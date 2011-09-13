2006ec729c6991130211387b21bf6c111461c333
Arnaud Bergeron <abergeron@gmail.com>
2011-09-13 17:27:59 -0400

    Add pycuda benchmark, and stop disabling gc because pycuda can't cope.

--- ndarray/bench.py
+++ ndarray/bench.py
@@ -7,6 +7,7 @@ theano.config.floatX = 'float32'
 try:
     import pycuda.autoinit
     from pycuda.compiler import SourceModule
+    from pycuda.elementwise import ElementwiseKernel
     from pycuda import gpuarray
 except ImportError:
     pass
@@ -15,27 +16,36 @@ try:
     import gen_elemwise
     from gen_elemwise import ElemwiseAlgo, call_elemwise, MyGpuNdArray
     import pygpu_ndarray as gpu_ndarray
-    gen_elemwise.gpu_ndarray = gpu_ndarray
 except ImportError:
     pass
 
-def timeit(f, lbl):
-
-    gc.disable()
-    t = time.time()
-    f()
-    est = time.time() - t
-    gc.enable()
-
-    loops = max(1, int(10**math.floor(math.log(10/est, 10))))
-
-    gc.disable()
-    t = time.time()
+def timeit_core(f, loops):
+# pycuda can't handle no gc beyond a certain number of calls
+#    gc.disable()
+    tstart = time.time()
     for _ in xrange(loops):
         f()
+    tend = time.time()
+#    gc.enable()
+    gc.collect()
 
-    print lbl, "(", loops, "loops ):", (time.time() - t)/loops, "s"
+    return (tend - tstart)/loops
+
+def timeit(f, lbl, n_tries=3):
+    gc.disable()
+    ts = time.time()
+    f()
+    te = time.time()
     gc.enable()
+    gc.collect()
+    est = te - ts
+
+    loops = max(1, int(10**math.floor(math.log(10/est, 10))))
+    
+    mintime = min(timeit_core(f, loops) for _ in xrange(n_tries))
+    
+    print lbl, "(", loops, "loops ):", mintime, "s ( best of", n_tries, ")"
+    return mintime
 
 numpy_tmpl = r"""
 def numpy(%s):
@@ -97,9 +107,23 @@ def elemwise_helper(kern, vars):
 
     def ndarray(gvars):
         inputs = [gvars[k] for k in order]
-        gen_elemwise.pycuda_array = False
-        call_elemwise(fct, inputs, block=(inputs[0].shape[-1], 1, 1))
+#        gen_elemwise.pycuda_array = False
+        return call_elemwise(fct, inputs, block=(inputs[0].shape[-1], 1, 1))
     return ndarray
+
+def pycuda_helper(kern, vars):
+    # this does float32 only
+    order = vars.keys()
+    for k in order:
+        kern = kern.replace(k, k+'[i]')
+    kern = '__out[i] = '+kern
+    ek = ElementwiseKernel('float *__out,' + ','.join("float *%s"%(n,) for n in order), kern)
+    def pycuda(vars):
+        inputs = [vars[k] for k in order]
+        out = gpuarray.empty_like(inputs[0])
+        ek(out, *inputs)
+        return out
+    return pycuda
 
 class bench(object):
     def __init__(self, exp, **vars):
@@ -114,6 +138,7 @@ class bench(object):
         res = {}
         exec numexpr_code in numexpr_map, res
         self.numexpr = res['numexpr']
+        self.pycuda = pycuda_helper(self.exp, self.vars)
 
     def run(self):
         keys = self.vars.keys()
@@ -124,25 +149,33 @@ class bench(object):
         
     def one_try(self, vars):
         timeit(lambda: self.empty(vars), "baseline")
-        timeit(lambda: self.numpy(**vars), "numpy")
+        ref = self.numpy(**vars)
+        timeit(lambda: self.numpy(**vars), "numpy   ")
         # to cache the expression, since we don't want to time the compile
         try:
-            self.numexpr(vars)
-            timeit(lambda: self.numexpr(vars), "numexpr")
+            res = self.numexpr(vars)
+            assert (res == ref).all()
+            timeit(lambda: self.numexpr(vars), "numexpr ")
         except Exception, e:
             print "numexpr: error", e
             
         try:
             gvars = dict((k, gpu_ndarray.GpuNdArrayObject(v)) for k,v in vars.iteritems())
             self.ndarray = elemwise_helper(self, gvars)
-            self.ndarray(gvars)
-            timeit(lambda: self.ndarray(gvars), "ndarray")
+            res = self.ndarray(gvars)
+            assert (res == ref).all()
+            timeit(lambda: self.ndarray(gvars), "ndarray ")
         except Exception, e:
             print "ndarray: error", e
         try:
-            timeit(lambda: self.pycuda(vars), "pycuda")
+            pvars = dict((k, gpuarray.to_gpu(v)) for k,v in vars.iteritems())
+            res = self.pycuda(pvars)
+            assert (res.get() == ref).all()
+            timeit(lambda: self.pycuda(pvars), "pycuda  ")
         except Exception, e:
             print "pycuda: error", e
+            import traceback
+            traceback.print_exc()
 
 series = bench("2*sin(a)-sin(2*a)+2/3.0*sin(3*a)-1/2.0*sin(4*a)+2/5.0*sin(5*a)-1/3.0*sin(6*a)+2/7.0*sin(7*a)", a=((100,), (1000,), (100, 200)))
 ap1 = bench("a+1", a=((100,), (1000,), (100, 200)))
@@ -152,9 +185,9 @@ b3 = bench("a**2 + b**2 + 2*a*b", a=((100,),), b=((100,),))
 if __name__ == "__main__":
     print ap1.exp
     ap1.run()
-    print b2.exp
-    b2.run()
-    print b3.exp
-    b3.run()
-    print series.exp
-    series.run()
+#    print b2.exp
+#    b2.run()
+#    print b3.exp
+#    b3.run()
+#    print series.exp
+#    series.run()
