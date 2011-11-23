d927ef2994090dd19f65050c76a7766e57597d99
Frederic <nouiz@nouiz.org>
2011-11-23 10:50:53 -0500

    pep8 fix.

--- ndarray/bench.py
+++ ndarray/bench.py
@@ -1,11 +1,18 @@
-import gc, time, math, re
+import gc
+import math
+import re
+import sys
+import time
+
 import numpy
 import numexpr
 import theano
 theano.config.floatX = 'float32'
 
+
 try:
-#    import theano.misc.pycuda_init# To allow selecting the gpu with THEANO_FLAGS
+# To allow selecting the gpu with THEANO_FLAGS
+#    import theano.misc.pycuda_init
     import pycuda.autoinit
     from pycuda.compiler import SourceModule
     from pycuda.elementwise import ElementwiseKernel
@@ -19,6 +26,7 @@ try:
     import pygpu_ndarray as gpu_ndarray
 except ImportError:
     pass
+
 
 def timeit_core(f, loops, nogc, sync_gpu):
     gc.collect()
@@ -38,7 +46,8 @@ def timeit_core(f, loops, nogc, sync_gpu):
             gc.enable()
         gc.collect()
 
-    return (tend - tstart)/loops
+    return (tend - tstart) / loops
+
 
 def timeit(f, lbl, n_tries=3, nogc=True, sync_gpu=False):
     # sync_gpu sync before/after for the first estimation call
@@ -62,12 +71,13 @@ def timeit(f, lbl, n_tries=3, nogc=True, sync_gpu=False):
     est = te - ts
 
     # This will generate a runtime between 1 and 10 seconds
-    loops = max(1, int(10**math.floor(math.log(10/est, 10))))
-    
+    loops = max(1, int(10 ** math.floor(math.log(10 / est, 10))))
+
     times = [timeit_core(f, loops, nogc, sync_gpu) for _ in xrange(n_tries)]
     mintime = min(times)
-    total_time = (numpy.asarray(times)*loops).sum()
-    print lbl, "(%d loops): %.3es (best of %d) run time %.2f"%(loops, mintime, n_tries, total_time)
+    total_time = (numpy.asarray(times) * loops).sum()
+    print lbl, "(%d loops): %.3es (best of %d) run time %.2f" % (
+        loops, mintime, n_tries, total_time)
     return mintime
 
 numpy_tmpl = r"""
@@ -102,14 +112,18 @@ typedef float npy_float32;
 typedef double npy_float64;
 """
 
+
 def empty(vars):
     return None
 
+
 def elemwise_helper(kern, vars, collapse=True):
     import theano
-    s = dict((k, theano.scalar.ScalarVariable(theano.scalar.Scalar(str(v.dtype)))) for k, v in vars.iteritems())
+    s = dict((k, theano.scalar.ScalarVariable(
+                theano.scalar.Scalar(str(v.dtype))))
+             for k, v in vars.iteritems())
 
-    theano_code = theano_tmpl%(kern.exp,)
+    theano_code = theano_tmpl % (kern.exp,)
     res = s.copy()
     exec theano_code in theano_smap, res
     sexp = res['exp']
@@ -117,7 +131,7 @@ def elemwise_helper(kern, vars, collapse=True):
     order = vars.keys()
     inputs_s = [s[k] for k in order]
     comp = theano.scalar.Composite(inputs_s, [sexp])
-    
+
     fct = MyGpuNdArray.gen_fct(theano.tensor.Elemwise(comp),
                                [MyGpuNdArray(i) for i in vars.values()],
                                vars.values()[0].ndim,
@@ -129,37 +143,45 @@ def elemwise_helper(kern, vars, collapse=True):
 
     return ndarray
 
+
 def var_iter(vars):
     """ return the initial value for the inputs.
-    
+
     This is done this way to ensure we keep only the mininum memory footprint.
     """
     keys_order = vars.keys()
     for shape_idx in range(len(vars.items()[0][1])):
         ret = {}
         for key in vars.keys():
-            shape = [v[0] if isinstance(v, (list, tuple)) else v for v in vars[key][shape_idx]]
-            stride = [v[1] if isinstance(v, (list, tuple)) else 1 for v in vars[key][shape_idx]]
+            shape = [v[0] if isinstance(v, (list, tuple)) else v
+                     for v in vars[key][shape_idx]]
+            stride = [v[1] if isinstance(v, (list, tuple)) else 1
+                      for v in vars[key][shape_idx]]
 
             val = numpy.random.random(shape).astype('float32')
             ret[key] = val, stride
         yield ret
+
 
 def pycuda_helper(kern, vars):
     # this does float32 only
     order = vars.keys()
     kern = re.sub(r'([0-9a-z.]+)\*\*([0-9a-z.]+)', r'pow(\1, \2)', kern)
     for k in order:
-        kern = kern.replace(k, k+'[i]')
-    kern = '__out[i] = '+kern
-    ek = ElementwiseKernel('float *__out,' + ','.join("const float *%s"%(n,) for n in order), kern)
+        kern = kern.replace(k, k + '[i]')
+    kern = '__out[i] = ' + kern
+    ek = ElementwiseKernel('float *__out,' + ','.join("const float *%s" % (n,)
+                                                      for n in order), kern)
+
     def pycuda(vars, out=None):
         inputs = [vars[k] for k in order]
-        if out is None or out.dtype != inputs[0].dtype or out.shape != inputs[0].shape:
+        if (out is None or out.dtype != inputs[0].dtype or
+            out.shape != inputs[0].shape):
             out = gpuarray.empty_like(inputs[0])
         ek(out, *inputs)
         return out
     return pycuda
+
 
 def apply_strides(vars):
     vars2 = {}
@@ -167,16 +189,17 @@ def apply_strides(vars):
         vars2[k] = val[tuple(slice(None, None, st) for st in stride)]
     return vars2
 
+
 class bench(object):
     def __init__(self, exp, **vars):
         self.exp = exp
         self.vars = vars
         self.empty = empty
-        numpy_code = numpy_tmpl%(','.join(vars.keys()), exp)
+        numpy_code = numpy_tmpl % (','.join(vars.keys()), exp)
         res = {}
         exec numpy_code in numpy_map, res
         self.numpy = res['numpy']
-        numexpr_code = numexpr_tmpl%(exp,)
+        numexpr_code = numexpr_tmpl % (exp,)
         res = {}
         exec numexpr_code in numexpr_map, res
         self.numexpr = res['numexpr']
@@ -195,7 +218,7 @@ class bench(object):
         if retval is not None:
             retval['numpy'] = t
         return t
-    
+
     def try_numexpr(self, vars, ref=None, retval=None):
         # to cache the expression, since we don't want to time the compile
         try:
@@ -212,24 +235,29 @@ class bench(object):
             import traceback
             traceback.print_exc()
 
-    def try_compyte(self, vars, ref=None, retval=None, reuse_output=False, collapse=True):
+    def try_compyte(self, vars, ref=None, retval=None,
+                    reuse_output=False, collapse=True):
         try:
-            # Take care! gpu_ndarray.GpuNdArrayObject will always generate a c contiguous
+            # Take care!
+            # gpu_ndarray.GpuNdArrayObject will always generate a c contiguous
             # memory region! This bypass the strides tests!
             gvars = dict((k, (gpu_ndarray.GpuNdArrayObject(v), st))
-                         for k,(v,st) in vars.iteritems())
+                         for k, (v, st) in vars.iteritems())
             gvars = apply_strides(gvars)
             self.ndarray = elemwise_helper(self, gvars, collapse=collapse)
             res = self.ndarray(gvars)
-            nd_col, info = gen_elemwise.elemwise_collapses(gvars.values(),[res])
-            print 'Size/Dimensions/Shapes after collapse:', numpy.prod(res.shape), nd_col, info[0][:nd_col]#, numpy.asarray(info[1])[:,:nd_col]
+            nd_col, info = gen_elemwise.elemwise_collapses(gvars.values(),
+                                                           [res])
+            print 'Size/Dimensions/Shapes after collapse:', numpy.prod(
+                res.shape), nd_col, info[0][:nd_col]
             if ref is not None:
                 assert numpy.allclose(res, ref)
             if reuse_output:
                 out = res
             else:
                 out = None
-            t = timeit(lambda: self.ndarray(gvars, out), "compyte ", nogc=False, sync_gpu=True)
+            t = timeit(lambda: self.ndarray(gvars, out), "compyte ",
+                       nogc=False, sync_gpu=True)
             if retval is not None:
                 retval['compyte'] = t
             return t
@@ -240,8 +268,10 @@ class bench(object):
 
     def try_pycuda(self, vars, ref=None, retval=None, reuse_output=False):
         try:
-            assert (numpy.asarray([st for k,(v,st) in vars.iteritems()])==1).all()
-            pvars = dict((k, gpuarray.to_gpu(v)) for k,(v,st) in vars.iteritems())
+            assert (numpy.asarray([st for k, (v, st) in vars.iteritems()]) ==
+                    1).all()
+            pvars = dict((k, gpuarray.to_gpu(v))
+                         for k, (v, st) in vars.iteritems())
             res = self.pycuda(pvars)
             if ref is not None:
                 assert numpy.allclose(res.get(), ref)
@@ -249,7 +279,8 @@ class bench(object):
                 out = res
             else:
                 out = None
-            t = timeit(lambda: self.pycuda(pvars, out), "pycuda  ", nogc=False, sync_gpu=True)
+            t = timeit(lambda: self.pycuda(pvars, out), "pycuda  ",
+                       nogc=False, sync_gpu=True)
             if retval is not None:
                 retval['pycuda'] = t
             return t
@@ -257,7 +288,7 @@ class bench(object):
             print "pycuda: error", e
             import traceback
             traceback.print_exc()
-    
+
     def one_try(self, vars):
         retval = {}
         timeit(lambda: self.empty(vars), "baseline")
@@ -271,19 +302,25 @@ class bench(object):
 
         return retval
 
+
 def prod(seq):
     s = 1
     for x in seq:
         s *= x
     return s
 
-MARKERS = ['+', '*', ',', '.', '1', '2', '3', '4', '<', '>', 'D', 'H', '^', '_', 'd', 'h', 'o', 'p', 's', 'v', 'x', '|']
-COLORS = ['blue', 'green', 'red', 'cyan', 'magenta', 'grey', 'yellow', 'k', 'orange', 'amber', 'purple', 'pink', '#FFAABB', '#FFAAFF']
+
+MARKERS = ['+', '*', ',', '.', '1', '2', '3', '4', '<', '>', 'D', 'H',
+           '^', '_', 'd', 'h', 'o', 'p', 's', 'v', 'x', '|']
+COLORS = ['blue', 'green', 'red', 'cyan', 'magenta', 'grey', 'yellow',
+          'k', 'orange', 'amber', 'purple', 'pink', '#FFAABB', '#FFAAFF']
+
+
 ##FFAABB, #FFAAFF
 def make_graph(name, b, msa, times={}):
     ext = 'pdf'
     import matplotlib
-    matplotlib.use(ext, warn=False) # maybe Agg or Cairo
+    matplotlib.use(ext, warn=False)  # maybe Agg or Cairo
     import matplotlib.pyplot as plt
     print 'Start graph', name
     idx = 0
@@ -299,7 +336,8 @@ def make_graph(name, b, msa, times={}):
             assert len(times[lbl]) == len(shapes)
             yvals = numpy.asarray(times[lbl])
         for vals_strides in var_iter(vars):
-            xvals.append(prod(numpy.asarray(vals_strides.values()[0][0].shape)/
+            xvals.append(prod(numpy.asarray(
+                        vals_strides.values()[0][0].shape) /
                               vals_strides.values()[0][1]))
             if lbl not in times:
                 #ref = b.numpy(**vals)
@@ -307,7 +345,7 @@ def make_graph(name, b, msa, times={}):
                 yvals.append(m(b, vals_strides, ref=ref))
         print "nb elem", xvals
         print "times", yvals
-        yvals = [y*1e6 for y in yvals]
+        yvals = [y * 1e6 for y in yvals]
         assert len(xvals) == len(yvals)
         plt.semilogx(xvals, yvals, label=lbl,
                      color=COLORS[idx], marker=MARKERS[idx])
@@ -316,7 +354,7 @@ def make_graph(name, b, msa, times={}):
     plt.ylabel('time (us)')
     plt.xlabel('number of elements')
     plt.title(b.exp)
-    filename = name+'.'+ext
+    filename = name + '.' + ext
     plt.savefig(filename)
     print "saved in file", filename
     plt.cla()
@@ -325,34 +363,65 @@ def make_graph(name, b, msa, times={}):
 #Too much mem on oolong: (1e6,), (1e8)))
 #shapes = ((100,), (100000,), (100,1000), (100,100,10),(100,10,10,10))
 #shapes = ((100,10,10,10),)
-shapes = ((1e2,), (1e3,))#, (1e4,), (1e5,), (1e6,), (1e7,))
+shapes = ((1e2,), (1e3,))  # , (1e4,), (1e5,), (1e6,), (1e7,))
 ap1 = bench("a+1", a=shapes)
 apb = bench("a+b", a=shapes, b=shapes)
 b2 = bench("2*a + 3*b", a=((100,),), b=((100,),))
 b3 = bench("a**2 + b**2 + 2*a*b", a=((100,),), b=((100,),))
 b4 = bench("2*a + b**10", a=((100,),), b=((100,),))
-series = bench("2*sin(a)-sin(2*a)+2/3.0*sin(3*a)-1/2.0*sin(4*a)+2/5.0*sin(5*a)-1/3.0*sin(6*a)+2/7.0*sin(7*a)",
+series = bench("2*sin(a)-sin(2*a)+2/3.0*sin(3*a)-1/2.0*sin(4*a)+2/5.0*"
+               "sin(5*a)-1/3.0*sin(6*a)+2/7.0*sin(7*a)",
                a=((100,), (1000,), (100, 200)))
 
 
 def gen_submited_paper_pdf():
     ap1_no_alloc_submit_biglearn_paper = {
-        'pycuda': [2.263465166091919e-05, 2.2561240196228026e-05, 5.0088729858398435e-05, 0.00024293808937072753, 0.00048887078762054439],
-        'compyte 1d contiguous': [4.8440003395080566e-05, 4.8550620079040524e-05, 5.4675912857055667e-05, 0.00027481579780578615, 0.00056338310241699219],
-        'compyte 4d contiguous': [6.9326648712158203e-05, 6.9620189666748047e-05, 7.0056605339050297e-05, 0.00027470300197601316, 0.00056341369152069088],
-        'compyte 4d contiguous not collapsed': [9.3558812141418462e-05, 9.4482517242431646e-05, 0.00029041900634765627, 0.00144551682472229, 0.002890136957168579],
-        'compyte 4d strided outer(2d after collapse)': [8.9702391624450685e-05, 9.054498672485351e-05, 0.00010384140014648437, 0.00051974868774414061, 0.00092990517616271969],
+        'pycuda':
+            [2.263465166091919e-05, 2.2561240196228026e-05,
+             5.0088729858398435e-05, 0.00024293808937072753,
+             0.00048887078762054439],
+        'gpu nd array 1d contiguous':
+            [4.8440003395080566e-05, 4.8550620079040524e-05,
+             5.4675912857055667e-05, 0.00027481579780578615,
+             0.00056338310241699219],
+        'gpu nd array 4d contiguous':
+            [6.9326648712158203e-05, 6.9620189666748047e-05,
+             7.0056605339050297e-05, 0.00027470300197601316,
+             0.00056341369152069088],
+        'gpu nd array 4d contiguous not collapsed':
+            [9.3558812141418462e-05, 9.4482517242431646e-05,
+             0.00029041900634765627, 0.00144551682472229,
+             0.002890136957168579],
+        'gpu nd array 4d strided outer(2d after collapse)':
+            [8.9702391624450685e-05, 9.054498672485351e-05,
+             0.00010384140014648437, 0.00051974868774414061,
+             0.00092990517616271969],
         }
 
     a2pb2p2ab_no_alloc_no_alloc_submit_biglearn_paper = {
-        'pycuda': [2.3609220981597899e-05, 2.3830621242523194e-05, 9.885969161987305e-05, 0.00047158560752868654, 0.00093993830680847172],
-        'compyte 1d contiguous': [5.5107808113098146e-05, 5.5577740669250487e-05, 0.00011780040264129639, 0.00053009800910949702, 0.0010585570335388183],
-        'compyte 4d contiguous': [8.4473395347595213e-05, 8.4439301490783696e-05, 0.00011779539585113526, 0.00053011291027069086, 0.0010585739612579345],
-        'compyte 4d contiguous not collapsed': [0.00012182121276855469, 0.0001221674919128418, 0.00034283380508422852, 0.0016802849769592286, 0.0033540680408477783],
-        'compyte 4d strided outer(2d after collapse)': [0.00011260590553283692, 0.00011380398273468018, 0.00021288449764251708, 0.00092618393898010257, 0.0018587779998779296],
+        'pycuda': [2.3609220981597899e-05, 2.3830621242523194e-05,
+                   9.885969161987305e-05, 0.00047158560752868654,
+                   0.00093993830680847172],
+        'gpu nd array 1d contiguous':
+            [5.5107808113098146e-05, 5.5577740669250487e-05,
+             0.00011780040264129639, 0.00053009800910949702,
+             0.0010585570335388183],
+        'gpu nd array 4d contiguous':
+            [8.4473395347595213e-05, 8.4439301490783696e-05,
+             0.00011779539585113526, 0.00053011291027069086,
+             0.0010585739612579345],
+        'gpu nd array 4d contiguous not collapsed':
+            [0.00012182121276855469, 0.0001221674919128418,
+             0.00034283380508422852, 0.0016802849769592286,
+             0.0033540680408477783],
+        'gpu nd array 4d strided outer(2d after collapse)':
+            [0.00011260590553283692, 0.00011380398273468018,
+             0.00021288449764251708, 0.00092618393898010257,
+             0.0018587779998779296],
         }
 
-    shapes_1d = [(100,), (1000,), (10000,), (100000,), (1000000,), (5000000,), (10000000,), (50000000,)]
+    shapes_1d = [(100,), (1000,), (10000,), (100000,), (1000000,),
+                 (5000000,), (10000000,), (50000000,)]
     shapes_2d = [(10, 10), (100, 10), (100, 100), (1000, 100), (1000, 1000),
                  (5000, 1000), (10000, 1000), (50000, 1000)]
     shapes_3d = [(10, 10, 1), (10, 10, 10), (100, 10, 10), (100, 100, 10),
@@ -409,7 +478,6 @@ def gen_submited_paper_pdf():
                            ((1000, 2), (200, 2), (20, 2), (20, 2)),
                            ((200, 2), (200, 2), (200, 2), (20, 2)),
                            ]
-                           #((1000, 2), (200, 2), (200, 2), (20, 2))] # 3G per object
 
     # Remove the case 100 et 1000 elements
     all_shapes = [shapes_1d,
@@ -438,23 +506,29 @@ def gen_submited_paper_pdf():
     if True:
         for x in all_shapes:
             for shape in x:
-                if numpy.prod([ s if not isinstance(s, (tuple,list)) else s[0] for s in shape])*4>700e6:
+                if numpy.prod([s if not isinstance(s, (tuple, list))
+                               else s[0] for s in shape]) * 4 > 700e6:
                     x.remove(shape)
 
     msa2 = [('pycuda',
-             lambda b, vals, ref: b.try_pycuda(vals, reuse_output=True, ref=ref),
+             lambda b, vals, ref: b.try_pycuda(vals, reuse_output=True,
+                                               ref=ref),
              shapes_1d),
-            ('compyte 1d contiguous',
-             lambda b, vals, ref: b.try_compyte(vals, reuse_output=True, ref=ref),
+            ('gpu nd array 1d contiguous',
+             lambda b, vals, ref: b.try_compyte(vals, reuse_output=True,
+                                                ref=ref),
              shapes_1d),
-            ('compyte 4d contiguous',
-             lambda b, vals, ref: b.try_compyte(vals, reuse_output=True, ref=ref),
+            ('gpu nd array 4d contiguous',
+             lambda b, vals, ref: b.try_compyte(vals, reuse_output=True,
+                                                ref=ref),
              shapes_4d),
-            ('compyte 4d contiguous not collapsed',
-             lambda b, vals, ref: b.try_compyte(vals, reuse_output=True, ref=ref, collapse=False),
+            ('gpu nd array 4d contiguous not collapsed',
+             lambda b, vals, ref: b.try_compyte(vals, reuse_output=True,
+                                                ref=ref, collapse=False),
              shapes_4d),
-            ('compyte 4d strided outer(2d after collapse)',
-             lambda b, vals, ref: b.try_compyte(vals, reuse_output=True, ref=ref),
+            ('gpu nd array 4d strided outer(2d after collapse)',
+             lambda b, vals, ref: b.try_compyte(vals, reuse_output=True,
+                                                ref=ref),
              shapes_4d_strided_outer),
             ]
 
@@ -524,7 +598,8 @@ if __name__ == "__main__":
                            ((1000, 2), (200, 2), (20, 2), (20, 2)),
                            ((200, 2), (200, 2), (200, 2), (20, 2)),
                            ]
-                           #((1000, 2), (200, 2), (200, 2), (20, 2))] # 3G per object
+                           # 3G per object
+                           #((1000, 2), (200, 2), (200, 2), (20, 2))]
 
     # Remove the case 100 et 1000 elements
     all_shapes = [shapes_1d,
